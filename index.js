7b2837261a649e94d53339d21f0e87dc6c4a6b25
Héctor Villa <hpizarro@Hectors-MacBook-Pro.local>
2019-11-05 08:39:34 -0500

    Added morgan and colors support

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import app from "./src/api/app";
+import colors from 'colors';
+import morgan from 'morgan';
+
 const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "/src/config/.env") });
+require("dotenv").config({ path: path.join(__dirname,"./src/config/.env") });
 
 /*
     @apiVersion = Current Version Running for REST API
@@ -13,6 +16,9 @@ const apiVersion = process.env.API_VERSION;
 const port = process.env.PORT || 9000;
 const appMode = process.env.MODE;
 
+//Adds morgan logger support
+app.use(morgan('dev'));
+
 //Gets the current version supported for the API
 app.get(`/api/version`, function(req, res) {
   res.status(200).send(`You are using API Version:${apiVersion}`);
@@ -20,6 +26,6 @@ app.get(`/api/version`, function(req, res) {
 
 app.listen(port, function() {
   console.log(
-    `${appMode} Server running on http://localhost:${port} Ver.${apiVersion}`
+    colors.blue(`${appMode} Server running on http://localhost:${port} Ver.${apiVersion}`)
   );
 });
